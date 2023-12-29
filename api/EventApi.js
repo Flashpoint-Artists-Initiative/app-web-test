e6708c7337a25dd197ce26a618521fdfae1a2ed4
Aaron Averill <aaron@expensepath.com>
2023-12-29 18:23:00 -0500

    Events api

---
+++ api/EventApi.js
@@ -0,0 +1,9 @@
+import { ApiUrl } from './env.js'
+import ApiBase from './ApiBase.js'
+import { session } from '../model/Session.js'
+
+export default class EventApi extends ApiBase {
+    static async getEvents() {
+        return await this.get(new URL(`${ApiUrl}events`), session.jwtToken)
+    }
+}
