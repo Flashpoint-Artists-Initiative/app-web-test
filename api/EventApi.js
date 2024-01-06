51cc809786c8f66b1c3a63f86b5b86379583c7f4
Aaron Averill <aaron@expensepath.com>
2024-01-05 23:08:07 -0500

    Event page

--- api/EventApi.js
+++ api/EventApi.js
@@ -6,8 +6,14 @@ export default class EventApi extends ApiBase {
     static async getEvents() {
         return await this.get(new URL(`${ApiUrl}events`), session.jwtToken)
     }
-    static async getEvent(id) {
-        return await this.get(new URL(`${ApiUrl}events/${id}`), session.jwtToken)
+    static async getEvent(id, options) {
+        var url = new URL(`${ApiUrl}events/${id}`)
+        if (options) {
+            for(const param in options) {
+                url.searchParams.set(param, options[param])
+            }
+        }
+        return await this.get(url, session.jwtToken)
     }
     static async search(params) {
         return await this.post(new URL(`${ApiUrl}events/search`), params, session.jwtToken)
--- components/PageEvent.js
+++ components/PageEvent.js
@@ -13,7 +13,7 @@ const template = `
                 {{else if fetch.notAuthorized}}
                 Not Authorized
                 {{else}}
-                {{fetch.error}}
+                {{fetch.error}} [{{fetch.status}}]
                 {{/if}}
             </h2>
         {{else}}
@@ -27,6 +27,47 @@ const template = `
             </div>
             {{#if event.location}}<h3 class="mt-2 text-truncate">{{event.location}}</h3>{{/if}}
             <div class="text-truncate">{{event.startDate}} - {{event.endDate}}</div>
+            <div class="d-flex align-center my-2">
+                <i class="material-icons text-blue mr-2">history</i>
+                {{#if event.ended}}<span>This event has ended</span>{{/if}}
+                {{#if event.happeningNow}}<span>Happening now!</span>{{/if}}
+                {{#if event.daysUntil}}<span>{{event.daysUntil}} days until event</span>{{/if}}
+            </div>
+            <h1>Tickets</h1>
+            {{#if event.noneAvailable}}<h3 class="text-blue">No tickets available for this event yet</h3>{{/if}}
+            {{#if event.saleEnded}}<h3 class="text-blue">Ticket sales ended</h3>{{/if}}
+            {{#if event.availableOn}}<h3 class="text-blue">Tickets go on sale {{event.availableOn}}</h3>{{/if}}
+            {{#if event.additionalAvailableOn}}<h3 class="text-blue">Additional tickets on sale {{event.additionalAvailableOn}}</h3>{{/if}}
+            {{#each event.currentTickets}}
+                <div class="app-ticket mdc-card mdc-card--outlined ma-2{{#if soldOut}} text-medium-emphasis{{/if}}">
+                    <div {{#if canBuy}}class="mdc-card__primary-action"{{/if}}>
+                    {{#if canBuy}}<a href="./ticket?id={{id}}" class="mdc-theme--on-surface">{{/if}}
+                        <div class="d-flex flex-column">
+                            <div class="d-flex b-bottom pa-4">
+                                <div class="d-flex flex-column mr-auto">
+                                    <h2 class="text-blue mt-0 mb-2">{{name}}</h2>
+                                    <div>
+                                        {{#if saleEnded}}Sale ended {{saleEnded}}
+                                        {{else if availableOn}}<span class="text-blue">Available on {{availableOn}}</span>
+                                        {{else if timeRemaining}}{{timeRemaining}}
+                                        {{else if soldOut}}<span class="text-red">Sold out</span>
+                                        {{/if}}
+                                    </div>
+                                </div>
+                                <div class="d-flex flex-column align-end">
+                                    <h2 class="my-0">{{#unless soldOut}}{{price}}{{/unless}}</h2>
+                                    {{#if showQtyAvailable}}<div class="text-red mt-2"><strong>ONLY {{qtyAvailable}} LEFT</strong></div>{{/if}}
+                                </div>
+                                
+                            </div>
+                            <div class="pa-4">{{description}}</div>
+                        </div>
+                        <div class="mdc-card__ripple"></div>
+                    {{#if canBuy}}</a>{{/if}}
+                    </div>
+                </div>
+            {{/each}}
+            </h3>
         {{/if}}
     {{else}}
         <div class="d-flex justify-center">
@@ -54,20 +95,106 @@ export class PageEvent extends HTMLElement {
     get templateData() {
         const dateOptions = {
             weekday: 'long',
-            month: 'numeric',
+            month: 'long',
             day: 'numeric',
             year: 'numeric'
         }
+        const dateTimeOptions = {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+
+        }
         let event = this.event?.data
         if (event) {
+            const now = new Date()
+            const startDate = new Date(event.start_date.slice(0,-1))
+            const endDate = new Date(event.end_date.slice(0,-1))
+            let daysUntil = 0
+            if (startDate > new Date()) {
+                daysUntil = Math.ceil((startDate.getTime() - now.getTime()) / (1000 * 3600 * 24)).toLocaleString()
+            }
+            const tickets = _.map(_.sortBy(event.ticket_types, 'name'), ticket => {
+                const saleStartDate = new Date(ticket.sale_start_date)
+                const saleEndDate = new Date(ticket.sale_end_date)
+
+                const ticketData = {
+                    id: ticket.id,
+                    name: ticket.name,
+                    description: ticket.description,
+                    active: ticket.active,
+                    deleted: ticket.deleted_at,
+                    price: '$' + ticket.price.toLocaleString(undefined, {minimumFractionDigits: 2}),
+                    startDate: saleStartDate,
+                    endDate: saleEndDate,
+                    reserved: ticket.quantity == 0,
+                    qtyAvailable: Math.max(0, ticket.quantity - ticket.purchased_tickets_count - ticket.reserved_tickets_count),
+                    canBuy: false
+                }
+                ticketData.showQtyAvailable = ticketData.qtyAvailable > 0 && ticketData.qtyAvailable < 10
+                if (saleEndDate < now) {
+                    ticketData.saleEnded = saleEndDate.toLocaleString()
+                } else if (saleStartDate > now) {
+                    ticketData.availableOn = saleStartDate.toLocaleString()
+                } else {
+                    if (ticketData.qtyAvailable) {
+                        ticketData.canBuy = true
+                        const minutes = Math.ceil(saleEndDate.getTime() - now.getTime()) / (1000 * 60)
+                        if (minutes < 60) {
+                            ticketData.timeRemaining = Math.floor(minutes) + ' minutes left'
+                        } else if (minutes < 60*24) {
+                            ticketData.timeRemaining = Math.floor(minutes / 60) + ' hours left'
+                        } else {
+                            ticketData.timeRemaining = Math.floor(minutes / (60 * 24)).toLocaleString() + ' days left'
+                        }
+                    } else {
+                        ticketData.soldOut = true
+                    }
+                }
+                return ticketData
+            })
+            const pastTickets = _.filter(tickets, ticket => {
+                return ticket.active && !ticket.reserved && ticket.saleEnded
+            })
+            const availableTickets = _.filter(tickets, ticket => {
+                return ticket.active && !ticket.reserved && ticket.canBuy
+            })
+            const currentSoldOut = _.filter(tickets, ticket => {
+                return ticket.active && !ticket.reserved && ticket.soldOut
+            })
+            const futureTickets = _.filter(tickets, ticket => {
+                return ticket.active && !ticket.reserved && ticket.availableOn
+            })
+            const currentTickets = availableTickets.concat(currentSoldOut)
             event = {
                 id: event.id,
                 name: event.name,
                 location: event.location,
                 deleted: event.deleted_at,
                 inactive: !event.active,
-                startDate: new Date(event.start_date.slice(0,-1)).toLocaleString(undefined, dateOptions),
-                endDate: new Date(event.end_date.slice(0,-1)).toLocaleString(undefined, dateOptions)
+                startDate: startDate.toLocaleString(undefined, dateOptions),
+                endDate: endDate.toLocaleString(undefined, dateOptions),
+                ended: endDate < now,
+                happeningNow: startDate <= now && endDate >= now,
+                daysUntil: daysUntil,
+                currentTickets: currentTickets
+            }
+            if (pastTickets.length && !currentTickets.length) {
+                event.saleEnded = true
+            }
+            if (futureTickets.length && !availableTickets.length) {
+                const firstSaleDate = _.minBy(futureTickets, 'startDate').startDate.toLocaleString(undefined, dateTimeOptions)
+                if (pastTickets.length || currentSoldOut.length) {
+                    event.additionalAvailableOn = firstSaleDate
+                } else {
+                    event.availableOn = firstSaleDate
+                }
+            }
+            if (!pastTickets.length && !currentTickets.length && !futureTickets.length) {
+                event.noneAvailable = true
             }
         }
         return {
@@ -89,7 +216,13 @@ export class PageEvent extends HTMLElement {
             }
             this.refresh()
             const id = new URLSearchParams(window.location.search).get('id');
-            const response = await EventApi.getEvent(id)
+            const options = {
+                include: 'ticketTypes'
+            }
+            if (session.getRoles().admin) {
+                options.include = 'ticketTypes,purchasedTickets,reservedTickets'
+            }
+            const response = await EventApi.getEvent(id, options)
             const data = await response.json()
             this.fetch = {
                 done: true,
