c529c982b3a083662d020311c8b26966c7874dd5
Aaron Averill <aaron@expensepath.com>
2024-01-08 19:04:21 -0500

    Add option to set hour

--- js/components/DatePicker.js
+++ js/components/DatePicker.js
@@ -3,9 +3,12 @@ import { Select } from './mdc/Select.js'
 const template = `
 <div class="d-flex flex-column">
     <div class="d-flex">
-        <mdc-select surface-fixed class="field-datepicker_month flex-grow-1 flex-shrink-1 pr-1"></mdc-select>
+        <mdc-select surface-fixed class="field-datepicker_month flex-grow-1 flex-shrink-1"></mdc-select>
         <mdc-select surface-fixed class="field-datepicker_day flex-grow-1 flex-shrink-1 px-2"></mdc-select>
-        <mdc-select surface-fixed class="field-datepicker_year flex-grow-1 flex-shrink-1 pl-1"></mdc-select>
+        <mdc-select surface-fixed class="field-datepicker_year flex-grow-1 flex-shrink-1"></mdc-select>
+        {{#if hasHour}}
+        <mdc-select surface-fixed class="field-datepicker_hour flex-grow-1 flex-shrink-1 pl-2"></mdc-select>
+        {{/if}}
     </div>
     <div>{{dayName}}</div>
 </div>
@@ -17,15 +20,17 @@ export class DatePicker extends HTMLElement {
     }
     get templateData() {
         return {
+            hasHour: this.hasAttribute('select-hour'),
             dayName: this._date ? this._date.toLocaleString(undefined, {weekday: 'long'}) : ' '
         }
     }
     refresh() {
         this.innerHTML = Handlebars.compile(template)(this.templateData)
-        const monthDatepicker = this.querySelector('.field-datepicker_month')
-        const dayDatepicker = this.querySelector('.field-datepicker_day')
-        const yearDatepicker = this.querySelector('.field-datepicker_year')
-        monthDatepicker.items = [
+        const monthSelect = this.querySelector('.field-datepicker_month')
+        const daySelect = this.querySelector('.field-datepicker_day')
+        const yearSelect = this.querySelector('.field-datepicker_year')
+        const hourSelect = this.querySelector('.field-datepicker_hour')
+        monthSelect.items = [
             {text: 'Jan', value: 0},
             {text: 'Feb', value: 1},
             {text: 'Mar', value: 2},
@@ -40,7 +45,7 @@ export class DatePicker extends HTMLElement {
             {text: 'Dec', value: 11},
         ]
         if (this._date) {
-            monthDatepicker.selected = this._date.getMonth()
+            monthSelect.selected = this._date.getMonth()
         }
         if (this._date) {
             const month = this._date.getMonth()
@@ -53,19 +58,37 @@ export class DatePicker extends HTMLElement {
                 })
                 date.setDate(date.getDate()+1)
             }
-            dayDatepicker.items = dayItems
-            dayDatepicker.selected = this._date.getDate()
+            daySelect.items = dayItems
+            daySelect.selected = this._date.getDate()
         }
         if (this._date) {
             const yearItems = []
             for(let year = this.yearRange[0]; year <= this.yearRange[1]; year++) {
                 yearItems.push({text: year, value: year})
             }
-            yearDatepicker.items = yearItems
-            yearDatepicker.selected = this._date.getFullYear()
+            yearSelect.items = yearItems
+            yearSelect.selected = this._date.getFullYear()
+        }
+        if (hourSelect && this._date) {
+            const hourItems = []
+            for(let h = 0; h < 24; h++) {
+                let text
+                if (h == 0) {
+                    text = 'MIDNIGHT'
+                } else if (h == 12) {
+                    text = 'NOON'
+                } else if (h < 12) {
+                    text = h + ' AM'
+                } else {
+                    text = (h-12) + ' PM'
+                }
+                hourItems.push({text: text, value: h})
+            }
+            hourSelect.items = hourItems
+            hourSelect.selected = this._date.getHours()
         }
 
-        monthDatepicker.addEventListener('change', event => {
+        monthSelect.addEventListener('change', event => {
             if (!this._date) {
                 this._date = new Date(new Date().getFullYear(), event.detail.value, 1)
             } else {
@@ -74,12 +97,12 @@ export class DatePicker extends HTMLElement {
             this.refresh()
             this.dispatchEvent(new CustomEvent('change', {detail: {value: this._date}}))
         })
-        dayDatepicker.addEventListener('change', event => {
+        daySelect.addEventListener('change', event => {
             this._date.setDate(event.detail.value)
             this.refresh()
             this.dispatchEvent(new CustomEvent('change', {detail: {value: this._date}}))
         })
-        yearDatepicker.addEventListener('change', event => {
+        yearSelect.addEventListener('change', event => {
             this._date.setFullYear(event.detail.value)
             this.refresh()
             this.dispatchEvent(new CustomEvent('change', {detail: {value: this._date}}))
