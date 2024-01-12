ca873d504e67f6055e447cc8fe34e15e336b67a0
Aaron Averill <aaron@expensepath.com>
2024-01-11 20:53:33 -0500

    formatting fixes

--- js/components/dialog/AddReservedTicketDialog.js
+++ js/components/dialog/AddReservedTicketDialog.js
@@ -2,7 +2,6 @@ import ReservedTicket from '../../model/ReservedTicket.js'
 import { DatePicker } from '../DatePicker.js'
 import { DialogButton } from '../mdc/DialogButton.js'
 import { Select } from '../mdc/Select.js'
-//import { TextArea } from '../mdc/TextArea.js'
 import { TextField } from '../mdc/TextField.js'
 
 const MDCDialog = mdc.dialog.MDCDialog
@@ -34,7 +33,7 @@ const template = `
                     <label class="d-block pt-2">Email</label>
                     <mdc-textfield class="w-100" input-class="field-email" input-type="text"></mdc-textfield>
                     <label class="d-block pt-2">Name</label>
-                    <mdc-textfield class="w-100" input-class="field-name" input-type="text" autocomplete="off"></mdc-textfield>
+                    <mdc-textfield class="w-100" input-class="field-name" input-type="text" input-autocomplete="off"></mdc-textfield>
                     <label class="d-block pt-2">Expiration</label>
                     <mdc-select surface-fullwidth class="field-expiration_type w-100"></mdc-select>
                     <div class="mt-2">
@@ -49,7 +48,7 @@ const template = `
                     <div class="d-flex w-100 pt-2">
                         <mdc-select surface-fixed class="field-quantityType mr-2"></mdc-select>
                         <div class="quantity-block">
-                            <mdc-textfield input-class="field-quantity" input-type="number" autocomplete="off" class="d-block" style="width:6rem"></mdc-textfield>
+                            <mdc-textfield input-class="field-quantity" input-type="number" input-autocomplete="off" class="d-block" style="width:6rem"></mdc-textfield>
                         </div>
                     </div>
                 </div>
@@ -62,7 +61,7 @@ const template = `
                                 <span class="mdc-list-item__ripple"></span>
                                 <div class="d-flex align-center w-100 py-2">
                                     <div class="d-flex flex-column flex-grow-1 flex-shrink-1 text-left" style="min-width:5rem">
-                                        <h3 class="my-0">{{name}}</h3>
+                                        <h3 class="text-truncate my-0">{{name}}</h3>
                                         <div class="text-truncate">{{description}}</div>
                                     </div>
                                     <div class="text-right">
@@ -90,7 +89,7 @@ const ticketTypeButtonTemplate = `
 <div class="mdc-button__ripple"></div>
 <div class="d-flex align-center w-100">
     <div class="d-flex flex-column flex-grow-1 flex-shrink-1 text-left" style="min-width:5rem">
-        <h3 class="my-0">{{name}}</h3>
+        <h3 class="text-truncate my-0">{{name}}</h3>
         <div class="text-truncate">{{description}}</div>
     </div>
     <div class="text-right flex-shrink-0">
@@ -105,8 +104,9 @@ export class AddReservedTicketDialog extends HTMLElement {
     get templateData() {
         return {
             open: this.isOpen,
+            ticketTypes: this.ticketTypes,
             eventName: this.eventName,
-            ticketTypes: this.ticketTypes
+            reservedTicket: this.reservedTicket
         }
     }
     get expirationType() {
@@ -176,8 +176,7 @@ export class AddReservedTicketDialog extends HTMLElement {
             element.querySelector('.field-quantity').value = '1'
             this.refreshFormFields()
 
-            const ticketTypeButton = element.querySelector('.ticket-type-button')
-            ticketTypeButton.addEventListener('click', event => {
+            element.querySelector('.ticket-type-button').addEventListener('click', event => {
                 event.preventDefault()
                 this.page = 'ticketType'
             })
@@ -283,8 +282,8 @@ export class AddReservedTicketDialog extends HTMLElement {
     _page = 'main'
     firstSelect = true
     quantityType = '1'
-    eventName = ''
     ticketTypes = []
+    eventName = ''
     reservedTicket = new ReservedTicket()
 }
 customElements.define('add-reserved-ticket-dialog', AddReservedTicketDialog)
